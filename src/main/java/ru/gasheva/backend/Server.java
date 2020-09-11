a11c6846babad915a0cbf2b54a320cbd6a50c2fd
Tanya <gasheva_99@mail.ru>
2020-09-11 09:21:33 +0300

    Write and Read message

--- src/main/java/ru/gasheva/backend/Server.java
+++ src/main/java/ru/gasheva/backend/Server.java
@@ -5,6 +5,8 @@ import ru.gasheva.backend.jsonparser.JSONParser;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 
 public class Server {
     private ServerSocket server;
@@ -17,38 +19,24 @@ public class Server {
         this.message = message;
         try {
             server = new ServerSocket(6666);
-            server.setSoTimeout(10*2000);
+            server.setSoTimeout(40*1000);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
     private boolean handleWithSocket() throws IOException {
-        InputStream is = client.getInputStream();
-        OutputStream os = client.getOutputStream();
 
-        // Receiving
-        byte[] lenBytes = new byte[4];
-        is.read(lenBytes, 0, 4);
-        int len = (((lenBytes[3] & 0xff) << 24) | ((lenBytes[2] & 0xff) << 16) |
-                ((lenBytes[1] & 0xff) << 8) | (lenBytes[0] & 0xff));
-        byte[] receivedBytes = new byte[len];
-        is.read(receivedBytes, 0, len);
-        String received = new String(receivedBytes, 0, len);
 
-        System.out.println("Server received: " + received);
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(client.getOutputStream()), true);
+        System.out.println(message);
+        out.println(message);
 
-        // Sending
-        String toSend = "Echo: " + received;
-        byte[] toSendBytes = toSend.getBytes();
-        int toSendLen = toSendBytes.length;
-        byte[] toSendLenBytes = new byte[4];
-        toSendLenBytes[0] = (byte)(toSendLen & 0xff);
-        toSendLenBytes[1] = (byte)((toSendLen >> 8) & 0xff);
-        toSendLenBytes[2] = (byte)((toSendLen >> 16) & 0xff);
-        toSendLenBytes[3] = (byte)((toSendLen >> 24) & 0xff);
-        os.write(toSendLenBytes);
-        os.write(toSendBytes);
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        System.out.println(br.readLine());
+
+        out.close();
+        br.close();
         return false;
     }
     public boolean startServer() throws IOException {
